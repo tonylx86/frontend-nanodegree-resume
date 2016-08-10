ab2a7836515e504697bd1d7822bd176530f00655
Tony Lee <tonylx86@aliyun.com>
2016-08-10 11:38:05 +0800

    finish lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,78 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// var awesomeThoughts = "I am 李熙 and I am AWESOME!";
+// console.log(awesomeThoughts);
+// var formattedName = HTMLheaderName.replace("%data%", "Tony Lee"),
+//     formattedRole = HTMLheaderRole.replace("%data%", "Front-end Engineer");
+// $("#header").prepend(formattedRole).prepend(formattedName);
+var bio = {
+    "name":"Tony Lee",
+    "role":"Web Developer",
+    "contacts":{
+        "mobile":"18915978898",
+        "email":"tonylx86@aliyun.com",
+        "github":"johndoe",
+        "twitter":"@johndoe",
+        "location":"San Francisco"
+    },
+    "welcomeMessage":"lorem upsum dolor sit amet etc etc etc.",
+    "skills": [
+        "awesomeness","delivering things","cryogenic sleep", "saving the universe"
+    ],
+    "biopic":"images/fry.jpg"
+};
+
+var work = {
+    "jobs":[
+        {
+            "employer":"Microsoft",
+            "title":"software engineer",
+            "location":"London, UK",
+            "dates":"2001-2010",
+            "description":"develop Windows 7"
+        },
+        {
+            "employer":"Facebook",
+            "title":"software engineer",
+            "location":"Mountain View, CA",
+            "dates":"2010-2014",
+            "description":"develop React.js"
+        }
+    ]
+};
+
+var education = {
+    "schools":[
+        {
+            "name":"MIT",
+            "location":"Cambridge, MA",
+            "degree dates":"2004-2008",
+            "url":"www.mit.edu",
+            "majors":["CS"]
+        },
+        {
+            "name":"UCLA",
+            "location":"LA",
+            "degree dates":"2008-2011",
+            "url":"www.ucla.edu",
+            "majors":["cs","design"]
+        }
+    ],
+    "onlineCourses":[
+        {
+            "title":"Programming languages",
+            "school":"Coursera",
+            "dates":"2013",
+            "url":"www.Coursera.com"
+        },
+        {
+            "title":"Mechanics",
+            "school":"EDX",
+            "dates":"2015",
+            "url":"www.edx.org"
+        }
+    ]
+};
+
